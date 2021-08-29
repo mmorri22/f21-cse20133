1afb85e4a51b7395cf27531341b9475e45424238
Matthew Morrison <mmorri22@nd.edu>
2021-08-29 17:33:31 -0400

    Programming Challenge 01 Skeleton Code

---
+++ PC01/setup.sh
@@ -0,0 +1,6 @@
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob1.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob2.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob3.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob4.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Prob5.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC01/Makefile
