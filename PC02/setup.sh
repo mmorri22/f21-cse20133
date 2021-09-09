7a5433d63e51891c569fcc13349bf9a5e509c9eb
Matthew Morrison <mmorri22@nd.edu>
2021-09-09 13:40:32 -0400

    Update PC02

---
+++ PC02/setup.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC02/Prob1.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC02/Prob1_mod.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC02/Prob2_1.c
+wget https://raw.githubusercontent.com/mmorri22/f21-cse20133/main/PC02/Prob2_2.c
+rm setup.sh
